1eb2e8edc7197c547ad0fd908f175c636d13872d
trongduy2701 <2054052011duy@ou.edu.vn>
2022-10-22 13:50:33 +0700

    Kiem tra dieu kien button Cong

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -19,10 +19,13 @@ namespace Calculator
 
         private void btCong_Click(object sender, EventArgs e)
         {
-            double a = double.Parse(txtSoA.Text);
-            double b = double.Parse(txtSoB.Text);
-            double c = a + b;
-            txtKetQua.Text = c.ToString();
+            if (txtSoA.Text != String.Empty || txtSoB.Text != String.Empty)
+            {
+                double a = double.Parse(txtSoA.Text);
+                double b = double.Parse(txtSoB.Text);
+                double c = a + b;
+                txtKetQua.Text = c.ToString(); 
+            }
         }
 
         private void btTru_Click(object sender, EventArgs e)
